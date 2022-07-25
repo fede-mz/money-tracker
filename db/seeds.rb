a7dac2afcc17858c640823a72d0e90951b663cf4
Federico Muñoz <fede.mz@gmail.com>
2022-07-25 16:16:00 +0200

    adding some seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,15 +1,27 @@
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
 #
+require 'csv'
 
 user = User.create!(name: 'Federico', email: 'fede.mz@gmail.com', password: 'password1')
-['Efectivo', 'Banco Sabadell', 'Banco BBVA', 'Tarjeta Credito', 'Inversiones'].each do |account|
-  Account.create!(user: user, title: account, currency: user.primary_currency)
-end
-['Alquiler', 'Servicios', 'Salud', 'Educación', 'Super', 'Movilidad',
- 'Entretenimiento', 'Ropa', 'Trabajo', 'Impuestos/Tasas', 'Otros', 'Balance'].each do |category|
-  Category.create!(user: user, title: category)
-end
-['Comida', 'Hogar', 'Combustible', 'Arreglo'].each do |tag|
-  Tag.create!(user: user, title: tag)
+
+CSV.foreach(File.open('./db/seeds/cash_flow.csv'), headers: true) do |row|
+  account = Account.find_or_create_by!(
+    user: user, title: row['Account'], currency: user.primary_currency
+  )
+  category = Category.find_or_create_by!(
+    user: user, title: row['Category']
+  )
+  cash_flow = CashFlow.create!(
+    account: account,
+    category: category,
+    flow_date: row['Date'],
+    amount: Money.new(row['Amount'].to_f*100, user.primary_currency),
+    is_balance: row['Is For Balance?'] == 'yes'
+  )
+  row['Tags']&.split(';')&.each do |tag|
+    cash_flow.tags << Tag.find_or_create_by!(
+      user: user, title: tag
+    )
+  end
 end
